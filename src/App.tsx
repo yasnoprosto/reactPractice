7f40a9d5e6d8275c63c53e07ebb79a0646b6e583
Denis Pochinskiy <ircxenon1@gmail.com>
2023-05-27 10:04:02 +0500

    usd rub filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MoneyComponent from "./MoneyComponent";
+export type {BanknoteType, MoneyType}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type BanknoteType = "NOT" | "USD" | "RUB"
+
+type MoneyType = {
+    banknotes: string,
+    value: number,
+    number: string
 }
 
-export default App;
+export const App = () => {
+    debugger
+
+
+    const [money, setMoney] = useState([
+        {banknotes: 'USD', value: 100, number: ' a1234567890'},
+        {banknotes: 'USD', value: 50, number: ' z1234567890'},
+        {banknotes: 'RUB', value: 100, number: ' w1234567890'},
+        {banknotes: 'USD', value: 100, number: ' e1234567890'},
+        {banknotes: 'USD', value: 50, number: ' c1234567890'},
+        {banknotes: 'RUB', value: 100, number: ' r1234567890'},
+        {banknotes: 'USD', value: 50, number: ' x1234567890'},
+        {banknotes: 'RUB', value: 50, number: ' v1234567890'},
+    ])
+
+    let currentMoney: any = money;
+    const [banknote, setBanknote] = useState<BanknoteType>("NOT");
+
+    if (banknote === "NOT") {
+        currentMoney = money
+    }
+    if (banknote === "USD") {
+        currentMoney = money.filter((moneyItem: MoneyType) => moneyItem.banknotes === "USD")
+    }
+    if (banknote === "RUB") {
+        currentMoney = money.filter((moneyItem: MoneyType) => moneyItem.banknotes === "RUB")
+    }
+    const onClickFilterHandler = (buttonName: string, banknotes: BanknoteType) => {
+        setBanknote(banknotes);
+    }
+
+    return (
+        <MoneyComponent currentMoney={currentMoney} onClickFilterHandler={onClickFilterHandler}/>
+    );
+}
